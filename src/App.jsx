ef400997af1cabb3a99ef94c72c5d13f8732b47b
Pancrass Njoroge <wankenobi29@gmail.com>
2024-06-24 22:52:00 +0300

    adding files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import StudentInfo from './components/Navigation/StudentInfo'
+
+import './App.css'
+import { Avatar } from './components/ui/avatar'
+
+function App() {
+  
+
+  return (
+    <div >
+    <StudentInfo />
+    <Avatar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navigation/StudentInfo.jsx
@@ -0,0 +1,9 @@
+const StudentInfo = () => {
+    return (
+        <div className="h-3.5">
+          <h1>Student Information</h1>
+        </div>
+    );
+};
+
+export default StudentInfo;
